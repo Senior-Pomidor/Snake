445636358a1e9534322eadc664d3fe7ff724e2f6
Senior-Pomidor <bobylev.vladi@gmail.com>
2020-04-10 01:44:25 +0300

    First Lesson. Hello World!

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("First program on C#");
+            Console.ReadLine();
         }
     }
 }
