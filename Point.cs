bd0310d682bcefd8ea4c22ad6659163acd85645c
Senior-Pomidor <bobylev.vladi@gmail.com>
2020-04-11 16:48:44 +0300

    creatd new class Point and refactor Program

---
+++ Point.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Snake
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
