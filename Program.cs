c1c8821f7796a1424ccb12cff7507953b16d2596
Senior-Pomidor <bobylev.vladi@gmail.com>
2020-04-12 00:59:16 +0300

    experiment with class List

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Snake
 {
@@ -27,6 +28,23 @@ namespace Snake
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0); //удаляет элемент с индексом 0
+            // Console.WriteLine(numList[0]);
+
+            List<Point> plist = new List<Point>();
+            plist.Add(p1);
+            plist.Add(p2);
 
             Console.ReadLine();
         }
